5899a2886d78b5ed8017af3fa0bba0b8617f96f2
Charlotte <smit-charlotte@outlook.com>
2025-05-20 12:10:19 +0200

    update step1

--- src/assets/components/step1/Step1.jsx
+++ src/assets/components/step1/Step1.jsx
@@ -6,20 +6,22 @@ export default function Step1({ setStepSelected, nom, setNom, email, setEmail, p
 
         <div className="div-step1">
 
-                <h3>Personal info</h3>
-                <p>Please provide your name, phone number and email adress.</p>
+                <div className="step-texts border">
+                    <h3>Personal info</h3>
+                    <p>Please provide your name, phone number and email adress.</p>
+                </div>
 
-                <form>
+                <form className="border">
                     <label htmlFor="name">Your name is: {nom}</label><br/>
                     {/* on dit que lorsque l'on change la valeur de cet input, on change l'état de notre variable nom (car la valeur de cet input est le nom entré par l'utilisateur). On utilise un e.target pour bien sélectionner cet input. */}
-                    <input type="text" id="nom" value={nom} onChange={(e) => setNom(e.target.value)} placeholder='Vingt-six' /><br/>
+                    <input className="w-100" type="text" id="nom" value={nom} onChange={(e) => setNom(e.target.value)} placeholder='Vingt-six' /><br/>
                     <label htmlFor="email">Your email is: {email}</label><br/>
-                    <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="vingt-six@email.com" /><br/>
+                    <input className="w-100" type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="vingt-six@email.com" /><br/>
                     <label htmlFor="phone">Your phone number is: {phone}</label><br/>
-                    <input type="tel" id="phone" value={phone} onChangeCapture={(e) => setPhone(e.target.value)} placeholder='e.g. +1 234 567 890' />
+                    <input className="w-100" type="tel" id="phone" value={phone} onChangeCapture={(e) => setPhone(e.target.value)} placeholder='e.g. +1 234 567 890' />
                 </form>
 
-                <div className="div-button">
+                <div className="div-button border d-flex justify-content-end">
                     <button onClick={() => setStepSelected("step2")}>Next step</button>
                 </div>
 
