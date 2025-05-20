e26cf2c4657a0b1768f3b5f3c664aa3c127f2bc6
Charlotte <smit-charlotte@outlook.com>
2025-05-20 11:41:56 +0200

    update step1

--- src/assets/components/sidebar/Sidebar.jsx
+++ src/assets/components/sidebar/Sidebar.jsx
@@ -1,13 +1,13 @@
 import './sidebar.css'
 
-export default function Sidebar() {
+export default function Sidebar({ stepSelected }) {
 
     return(
 
         <>
             <div className="div-step d-flex gap-3">
                 <div className="div-number">
-                    <div className="wrap-number">
+                    <div className={`wrap-number ${stepSelected === "step1" ? "selected" : ""}`}>
                         <p className="pa-1">1</p>
                     </div>
                 </div>
@@ -16,15 +16,43 @@ export default function Sidebar() {
                     <p className="pa-3 m-0 p-0 ">YOUR INFO</p>
                 </div>
             </div>
-            <div className="div-step">
 
+            <div className="div-step d-flex gap-3">
+                <div className="div-number">
+                    <div className="wrap-number">
+                        <p className="pa-1">2</p>
+                    </div>
+                </div>
+                <div className="div-name-step d-flex flex-column">
+                    <p className="pa-2 m-0 p-0">STEP 2</p>
+                    <p className="pa-3 m-0 p-0 ">SELECT PLAN</p>
+                </div>
             </div>
-            <div className="div-step">
 
+            <div className="div-step d-flex gap-3">
+                <div className="div-number">
+                    <div className="wrap-number">
+                        <p className="pa-1">3</p>
+                    </div>
+                </div>
+                <div className="div-name-step d-flex flex-column">
+                    <p className="pa-2 m-0 p-0">STEP 3</p>
+                    <p className="pa-3 m-0 p-0 ">ADD-ONS</p>
+                </div>
             </div>
-            <div className="div-step">
 
+            <div className="div-step d-flex gap-3">
+                <div className="div-number">
+                    <div className="wrap-number">
+                        <p className="pa-1">4</p>
+                    </div>
+                </div>
+                <div className="div-name-step d-flex flex-column">
+                    <p className="pa-2 m-0 p-0">STEP 4</p>
+                    <p className="pa-3 m-0 p-0 ">SUMMARY</p>
+                </div>
             </div>
+
         </>
     )
 }
---
+++ src/assets/components/step1/Step1.jsx
@@ -0,0 +1,15 @@
+import './step1.css'
+
+export default function Step1() {
+
+
+    return(
+
+        <div className="div-step1 m-4">
+
+                <h3>Personal info</h3>
+                <p>Please provide your name, phone number and email adress.</p>
+
+        </div>
+    )
+}
