300f89369f9664f0670693b3d34acb3c3261908b
Charlotte <smit-charlotte@outlook.com>
2025-05-20 11:01:39 +0200

    update sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 import './App.css'
+import Sidebar from './assets/components/sidebar/Sidebar'
 
 function App() {
+
+  const [stepClicked, setStepClicked] = useState("Step 1")
 
   return (
 
@@ -10,7 +13,7 @@ function App() {
         <div className="div-all">
 
           <div className="div-sidebar">
-            
+            <Sidebar />
           </div>
 
           <div className="div-content">
---
+++ src/assets/components/sidebar/Sidebar.jsx
@@ -0,0 +1,30 @@
+import './sidebar.css'
+
+export default function Sidebar() {
+
+    return(
+
+        <>
+            <div className="div-step d-flex gap-3">
+                <div className="div-number">
+                    <div className="wrap-number">
+                        <p className="pa-1">1</p>
+                    </div>
+                </div>
+                <div className="div-name-step d-flex flex-column">
+                    <p className="pa-2 m-0 p-0">STEP 1</p>
+                    <p className="pa-3 m-0 p-0 ">YOUR INFO</p>
+                </div>
+            </div>
+            <div className="div-step">
+
+            </div>
+            <div className="div-step">
+
+            </div>
+            <div className="div-step">
+
+            </div>
+        </>
+    )
+}
